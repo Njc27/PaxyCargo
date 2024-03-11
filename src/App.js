740b348e200d3fdde0d851aec8c1efbf4846c41a
Njc27 <jc.n@northeastern.edu>
2024-03-11 01:40:34 -0400

    saved changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route,Navigate} from 'react-router-dom';
 import Home from './Components/home';
 import Track from './Components/trackOrder';
 import Nav from "./Components/Nav";
