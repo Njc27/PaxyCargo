dd558977f84ad0a31cdfc919375daa3b31f5a940
Njc27 <jc.n@northeastern.edu>
2024-03-11 01:39:16 -0400

    latest change

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ function App() {
        <Router>
         <Nav />
         {/* <Header/> */}
-      <Routes>
-          <Route exact path="/home" element={<Home />} />
-          <Route path="/track" element={<Track />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-         </Routes>
+        <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/track" element={<Track />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        </Routes>
       </Router>
     </div>
   );
